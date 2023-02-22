81ce97f50e9db4fc9f17f308b33ba323d6cadefc
marouan <devopscode9@gmail.com>
2023-02-22 04:58:48 +0100

    edit in Offers

--- js/script.js
+++ js/script.js
@@ -8,33 +8,40 @@ let bakbut3 = document.getElementById('bak-but-3')
 
 bak3.onmousemove = function () {
     bakbut3.style.background = "green"
+    bakbut3.style.color = "#fff"
     bakbut3.style.transition = "800ms"
     bakbut3.style.transform = "scale(1.08 , 1.08)"
 }
 bak3.onmouseleave = function () {
     bakbut3.style.background = "#f0f0f0"
+    bakbut3.style.color = "#000"
     bakbut3.style.transition = "1000ms"
     bakbut3.style.transform = "scale(1 , 1)"
 }
 
 bak2.onmousemove = function () {
     bakbut2.style.background = "green"
+    bakbut2.style.color = "#fff"
     bakbut2.style.transition = "800ms"
     bakbut2.style.transform = "scale(1.08 , 1.08)"
 }
 bak2.onmouseleave = function () {
     bakbut2.style.background = "#f0f0f0"
+    bakbut2.style.color = "#000"
     bakbut2.style.transition = "1000ms"
     bakbut2.style.transform = "scale(1 , 1)"
 }
 
 bak1.onmousemove = function () {
     bakbut1.style.background = "green"
+    bakbut1.style.color = "#fff"
     bakbut1.style.transition = "800ms"
     bakbut1.style.transform = "scale(1.1 , 1.08)"
 }
 bak1.onmouseleave = function () {
     bakbut1.style.background = "#f0f0f0"
+    bakbut1.style.color = "#000"
     bakbut1.style.transition = "1000ms"
     bakbut1.style.transform = "scale(1 , 1)"
 }
+
